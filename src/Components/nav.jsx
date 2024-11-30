637e6dedc6ff7241cf11f90fc2893c1be18bcd09
JeremiahID <jeremiahefe.je@gmail.com>
2024-11-30 20:12:59 +0100

    bootstrap file added

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 function Nav(){
     return (
-        <div className="navigation  ">
+        <div className="navigation d-md-block d-none ">
             <ul className=" d-flex   ">
                 <li ><a  href="http://" target="_blank" rel="noopener noreferrer"> E-shop </a></li>
                 <li><a href="http://" target="_blank" rel="noopener noreferrer"> Media </a></li>
