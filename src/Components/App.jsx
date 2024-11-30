29e7ccc2f58c54f37855ce119c1916bca08a76be
JeremiahID <jeremiahefe.je@gmail.com>
2024-11-30 20:03:38 +0100

    bootstrap file added

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -10,7 +10,7 @@ import '../css/App.css';
 function App(){
     return (
 
-        <div className=" bg-dark-subtle">
+        <div className=" ">
             <Header />
             <Hero />
             <Footer />
--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -4,9 +4,9 @@ import React from "react";
 
 function Nav(){
     return (
-        <div className="navigation">
-            <ul className=" d-flex">
-                <li><a href="http://" target="_blank" rel="noopener noreferrer"> E-shop </a></li>
+        <div className="navigation d-flex justify-content-center bg-dark  ">
+            <ul className=" d-flex bg-danger ">
+                <li ><a  href="http://" target="_blank" rel="noopener noreferrer"> E-shop </a></li>
                 <li><a href="http://" target="_blank" rel="noopener noreferrer"> Media </a></li>
                 <li><a href="http://" target="_blank" rel="noopener noreferrer"> Agenda </a></li>
                 <li><a href="http://" target="_blank" rel="noopener noreferrer"> concepts </a></li>
