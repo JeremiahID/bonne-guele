02edfd5436bd3a5d5b40ffc3917e5aae94768b65
JeremiahID <jeremiahefe.je@gmail.com>
2024-12-03 15:33:58 +0100

    form component rendering

--- src/Components/form.jsx
+++ src/Components/form.jsx
@@ -5,7 +5,7 @@ function Form(props){
         <div className="form  bg-danger">
             <form action="" method="post" className="flex flex-column  bg-success-subtle inherit">
                
-                <h1  className="text-uppercase fs-1 fw-light text-start bg-danger ">{props.name}</h1>
+                <h1  className="text-uppercase fw-light text-start">{props.name}</h1>
                 <p className="px-4">{props.textContent}</p>
 
             </form>
