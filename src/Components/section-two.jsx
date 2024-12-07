fcebda8066040f0f05bd2ee7756db4da0a040426
JeremiahID <jeremiahefe.je@gmail.com>
2024-12-07 17:54:15 +0100

    using arrow function in the map method

--- src/Components/section-two.jsx
+++ src/Components/section-two.jsx
@@ -2,24 +2,18 @@ import React from "react";
 import Details from "./shipping";
 import shippingStorage from "./shippingcontainer";
 
-
-// creating the function to be pass into the map method.
-function shippingItems(items){
-    return <Details 
-        svg = {items.svg}
-        name = {items.name}
-        innerText = {items.innerText}
-    />
-};
-
-
-
 function SectionTwo(){
     return(
         <div className="details my-4 row flex text-center ">
 
             {/* USING THE MAP METHOD */}
-            {shippingStorage.map(shippingItems)}
+            {shippingStorage.map(items =>{
+                return <Details 
+                        svg = {items.svg}
+                        name = {items.name}
+                        innerText = {items.innerText}
+                    />
+            })}
 
         </div>
     )
