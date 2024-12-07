57ae1dd2f35379c3de411a60616d539715900a5a
JeremiahID <jeremiahefe.je@gmail.com>
2024-12-07 17:37:26 +0100

    the map method applied to the section two component

--- src/Components/shippingcontainer.jsx
+++ src/Components/shippingcontainer.jsx
@@ -8,30 +8,23 @@ const shippingStorage = [
         svg: web,
         name :  'FREE SHIPPING',
         innerText : 'Across The African Union From 200$  purchase'
-
     },
-        
     {
         svg: loop,
         name :  'FREE RETURNS',
         innerText : '30 days to change your mind We will take care of everything'
     },
-
     {
         svg: creditcard,
         name :  'CREDIT CARDS',
         innerText : 'By Credit Card Secure Option'
     },
-
     {
         svg: chat,
         name :  'EXPRESS CUSTOMER SERVICE',
         innerText : 'Lightning Response from Monday - Friday'
     }
-
-
-
-]
+];
 
 
 export default shippingStorage;
