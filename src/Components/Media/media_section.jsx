22c93f00b9fa84aaf4bae8e4c1e89a38af685f31
JeremiahID <jeremiahefe.je@gmail.com>
2024-12-09 17:43:03 +0100

    media

--- src/Components/Media/media_section.jsx
+++ src/Components/Media/media_section.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function MediaSection(props){
     return (
-        <div className="col-4 flex border">
+        <div className="col-4 flex ">
             <div className=" flex flex-column  media-component ">
-                <div className=" media-component-img bg-danger">
+                <div className=" media-component-img ">
                     <img className="" src={props.img} alt="image"/>
                 </div>
-                <div className="media-component-link bg-danger-subtle">
+                <div className="media-component-link">
                     <a href="">{props.link} </a>
                 </div >
                 <div className="media-component-text">
