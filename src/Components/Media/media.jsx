120a9f910ab7b654c633f94559c5a03b0b3e4a06
JeremiahID <jeremiahefe.je@gmail.com>
2024-12-09 16:40:34 +0100

    media

--- src/Components/Media/media.jsx
+++ src/Components/Media/media.jsx
@@ -1,11 +1,13 @@
 import React  from "react";
 import Header from "../header";
+import MediaSection from "./media_section";
 
 
 function Media(){
     return (
         <div>
             <Header />
+            <MediaSection />
 
         </div>
     )
--- src/Components/Media/media_section.jsx
+++ src/Components/Media/media_section.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 function MediaSection(){
     return (
-        <div className="">
-            <div className="row">
+        <div className="row  bg-dark">
+            <div className="col-4">
+
 
 
             </div>
