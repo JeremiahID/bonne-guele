b386a1b6b5e5bdc55020978648199b7c6e638148
JeremiahID <jeremiahefe.je@gmail.com>
2024-12-11 17:20:30 +0100

    cart component created

---
+++ src/Components/cart.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import close from "../Images/close-cart.svg"
+import cart from "../Images/cart.svg"
+
+function Cart(){
+    return (
+        <div>
+             <div className="cart-wrapper position-absolute top-0 end-0 no-scroll" >
+
+                <div className="cart-container  position-absolute top-0 end-0 ">
+
+                    <div className="cart-head flex ">
+
+                        <div className="cart-icons-container flex justify-content-between  ">
+
+                            <div className="cart-logo flex justify-content-evenly  p-0"> 
+                                <img className=" close-cart-icon cart-icon " src={cart} alt="close cart icon" /> 
+
+                                <p className="text-capitalize fs-5 fw-bold m-0">cart</p>
+
+                            </div>
+
+
+                            <div className="close-cart">
+                                {/* <!-- CLOSE CART --> */}
+                                <img className=" close-cart-icon cart-icon mx-2" src={close} alt="close cart icon" />
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div className="cart-body flex flex-column">
+                        <p className="text-capitalize font-fc fs-6">Your cart is empty</p>
+
+                        <a href="" target="_blank" className="btn-a "> start shopping</a>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+};
+
+
+export default Cart;
