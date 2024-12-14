df9bd35d31ff96f7f26dd327ab13479c17427a8a
JeremiahID <jeremiahefe.je@gmail.com>
2024-12-14 18:04:18 +0100

    agenda component page

--- src/Components/Agenda/agenda.jsx
+++ src/Components/Agenda/agenda.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import Header from '../header';
+import Footer from '../footer';
+import SectionTwo from '../section-two';
+
 
 function Agenda(){
     return (
         <div>
             <Header />
 
-            <div className=''>
-                <div className=' bg-dark-subtle '>
+            <div className=' agenda-section-one-wrapper flex'>
+                <div className='agenda-section-one  m-0 flex flex-column justify-content-around'>
                     <p className=' flex'> 
-                        <a href="http://" target="_blank" rel="noopener noreferrer">
+                        <a href="http://" target="_blank" rel="noopener noreferrer" className='text-decoration-none text-black'>
                             Reception / 
                         </a>
                         <p className='font-fc p-0 m-0'>
                              Our collection for Tommorrow
                         </p> 
                     </p>
+
+                    <h1 className='text-uppercase font-fc fs-1 text-blue-dark'>Our collections of tomorrow </h1>
                 </div>
             </div>
+            
+            <SectionTwo />
+            <Footer />
 
         </div>
     )
@@ -25,4 +33,4 @@ function Agenda(){
 
 
 
-export default Agenda;
+export default Agenda;      
