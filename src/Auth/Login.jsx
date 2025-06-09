7797a6b287ef3e80b8dc9cb1b75513cb0408dd18
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2025-06-10 00:55:20 +0100

    Authorization component

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 function Login() {
     return(
-        <div className="Login-wrapper ">
-            <h1>tst</h1>
+        <div className="Login-wrapper d-flex justify-content-center align-items-center ">
+            <div className="Login-form">
+                <form action="" method="post">
+                    
+                </form>
+
+            </div>
         </div>
     )
 }
