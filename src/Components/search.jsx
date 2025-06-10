8a3af18533ea08a2f849bef58cd137d745f9707d
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2025-06-10 03:18:25 +0100

    search component

--- src/Components/search.jsx
+++ src/Components/search.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Search(){
     return (
-        <div className="search-wrapper">
-            
+        <div className="search-wrapper position-fixed top-0 end-0 d-flex justify-content-center align-items-center">
+            <h1>Search </h1>
         </div>
     )
 }
