e8493c3ea385ac4ef9416c6b347891431f4ad1d4
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2025-06-10 03:42:33 +0100

    search new component

--- src/Components/search.jsx
+++ src/Components/search.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import close from "../Images/close-cart.svg"
 
-export default function Search(){
+
+export default function Search(props){
     return (
         <div className="search-wrapper position-fixed top-0 end-0 d-flex justify-content-center align-items-center">
-            <h1>Search </h1>
+            <button className="search-close-button p-2 m-0" onClick={props.openSearch}>
+                    <img className=" close-cart-icon cart-icon text-light " src={close} alt="close cart icon" />
+            </button>
+
+            <input className="search-bar" type="search" name="search" id="serach" aria-label="search" placeholder="Search" />
         </div>
     )
-}
+}   
