13d339aa099fb088d7180728b90a31ce727d0c87
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2025-06-10 01:42:18 +0100

    Authorization component

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -7,7 +7,7 @@ function Login(props) {
     return(
         <div className="login-wrapper d-flex justify-content-center align-items-center ">
             <div className="login-form-container d-flex flex-column justify-content-between ">
-                <button className="svg-button login-button p-2 m-0 " onClick={props.closeLogin}>
+                <button className="svg-button login-button p-2 m-0 " onClick={props.openLogin}>
                     <img className=" close-cart-icon cart-icon " src={close} alt="close cart icon" />
                 </button>
                 <form action="" method="post" className="login-form">
