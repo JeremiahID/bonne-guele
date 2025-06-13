af3b6db6a27c15a57297d292dff4e3c12f41797b
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2025-06-13 11:43:48 +0100

    Eshop page development

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Hero(props){
     return(
-        <div className="hero-wrapper d-flex ">
+        <div className="hero-wrapper d-flex  ">
            <div className="hero">
             <img  src={props.heroImage} alt="hero-image"  />
+            <button type="button" className=" hero-button ">
+                <Link to="" className=" hero-link text-decoration-none text-white fw-bolder tracki "> 
+                    {props.buttonText}
+                </Link>
+            </button>
+
            </div>
         </div>
     )
--- src/Pages/Eshop/eshop.jsx
+++ src/Pages/Eshop/eshop.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import Hero from "../../Components/hero";
 import EshopHero from "../../Images/eshophero.webp"
+import { Link } from "react-router-dom";
 
-function Eshop(){
+function Eshop(props){
     return(
-        <>
-          <Hero  heroImage = {EshopHero} />  
-        </>
+        <div>
+          <Hero  heroImage = {EshopHero} buttonText = "FIND OUT" />  
+
+          <div className="collectionHero  d-flex" >
+            <div className="collection-hero-wrapper d-flex justify-content-center align-items-end">
+                <button type="button" className=" my-4 eshop-hero-button">
+                    <Link to="" className=" hero-link text-decoration-none text-white fw-bolder tracki "> 
+                        SEE THE COLLECTION
+                    </Link>
+                </button>
+            </div>
+          </div>
+
+            
+
+        </div>
     )
 };
 
