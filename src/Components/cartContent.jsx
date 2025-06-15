ad1f02287109c397db37da3126c188e64a4ab3a0
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2025-06-15 01:56:51 +0100

    cart page content build up

--- src/Components/cartContent.jsx
+++ src/Components/cartContent.jsx
@@ -1,3 +1,9 @@
+import deleteIcon from "../Images/delete.svg";
+import minus from "../Images/minus.svg";
+import add from "../Images/add.svg"
+
+
+
 export default function CartContent(){
     return (
         <div className="cart-body-wrapper">
@@ -7,26 +13,34 @@ export default function CartContent(){
                     <div className="cart-content-section-left">
                     
                     </div>
-                    <div className="cart-content-section-middle">
-
+                    <div className="cart-content-section-middle flex flex-column justify-content-between  align-items-start ">
+                        <p className="">
+                            Lui Vuitton
+                        </p>
+                        <p className="m-0">
+                            $120
+                        </p>
                     </div>
-                    <div className="cart-content-section-right">
-
-                    </div>
-                </div>
-                <div className="cart-content flex">
-                    <div className="cart-content-section-left">
-                    
-                    </div>
-                    <div className="cart-content-section-middle">
-
-                    </div>
-                    <div className="cart-content-section-right">
-
+                    <div className="cart-content-section-right d-flex flex-column">
+                        <div className="delete-icon flex justify-content-end align-items-start p-1">
+                            <img src={deleteIcon} alt="delete button" className="svg " />
+                        </div>
+                        <div className="add-remove-price flex justify-content-around align-items-center">
+                            <div>
+                                <button className="svg-button">
+                                    <img src={minus} alt="Minus-svg" className="svg" />
+                                </button>
+                            </div>
+                            <p className=" fs-5 pt-3">0</p>
+                            <div>
+                                <button className="svg-button">
+                                    <img src={add} alt="add-svg" className="svg" />
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 
-
             </div>
         </div>
     )
