18b8149a2b7ec828e4ee53429632b18b2cf94924
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2025-06-15 13:09:23 +0100

    navigation home link path

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -53,7 +53,7 @@ function Header(){
     return (
         <div className="header-container d-flex ">
             <div className="header d-flex position-fixed top-0 ">
-                <a href="/home" className="company-name flex justify-content-start">
+                <a href="/" className="company-name flex justify-content-start">
                     <img src={Logo} alt="Logo" className="Logo" />
                 </a>
                 
