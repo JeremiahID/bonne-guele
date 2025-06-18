bc6bea04c901f8464aa40224057cc2af785f0a9d
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2025-06-18 10:19:59 +0100

    Mobile Page development

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 function Footer(){
     return (
-        <div className='footer flex flex-column '>
-            <h1 className='text-uppercase fs-4  w-50 text-center'>"BonneGueule, clothes that carry you"</h1>
-            <ul className='flex justify-content-evenly py-4 footer-links'>
+        <div className='footer flex flex-column'>
+            <h1 className='text-uppercase w-100 text-center '>"BonneGueule, clothes that carry you"</h1>
+            <ul className='flex justify-content-evenly py-4 footer-links '>
                 <li><a className='fs-6 text-capitalize' href="http://" target="_blank" >Agenda</a></li>
                 <li><a className='fs-6 text-capitalize' href="http://" target="_blank" >Concept</a></li>
                 <li><a className='fs-6 text-capitalize' href="http://" target="_blank" >Lookbook</a></li>
                 <li><a className='fs-6 text-capitalize' href="http://" target="_blank" >Shops</a></li>
             </ul>
             <hr className='ft-line bg-dark'/>
-            <p className='fs-8'>
+            <p className='fs-8 text-center'>
               Cookie / GDPR Legal This site is protected by reCAPTCHA and Google's Privacy Policy and Terms of Service.
             </p>
-            <p className='fs-8'>All Right Reserved &copy; Design by Jeremiah Efe</p>
+            <p className='fs-8 text-center'>All Right Reserved &copy; Design by Jeremiah Efe</p>
         </div>
     );
 };
