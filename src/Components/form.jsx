a4c3384eeac4722b1b61e25c24286e76ce31e839
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2025-06-18 10:07:53 +0100

    Mobile Page development

--- src/Components/form.jsx
+++ src/Components/form.jsx
@@ -7,7 +7,7 @@ function Form(props){
             <form action="" method="post" className="flex flex-column  boutiques-content-left inherit">
                
                 <h1  className="text-uppercase fw-light text-start">{props.name}</h1>
-                <p className="px-4">{props.textContent}</p>
+                <p className="px-4 text-center">{props.textContent}</p>
                 <Btn 
                     innerText = 'OUR STORES'
                 />
--- src/Components/shippingDetails.jsx
+++ src/Components/shippingDetails.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Details(props){
     return (
-        <div className=" details-content col-3 flex justify-content-evenly flex-column">
+        <div className=" details-content col-md-3 col-sm-6 col-12 flex justify-content-evenly flex-column">
             
             <img className="details-img" src={props.svg} alt="svg"  />
             
