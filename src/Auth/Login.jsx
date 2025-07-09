130ecd91a0aed8be1a65c87014cc8c1c51674926
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2025-07-09 14:21:41 +0100

    commit first message

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -6,7 +6,6 @@ import GmailAuth from "./AccountAuth/gmailAuth";
 
 
 
-
 function Login(props) {
     return(
         <div className="account-wrapper d-flex justify-content-center align-items-center position-fixed top-0 end-0">
